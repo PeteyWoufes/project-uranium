06100593342da080b5a4d892c2c504099b3ee276
PeteyWoufes <30848697+PeteyWoufes@users.noreply.github.com>
2017-11-24 19:25:53 +0000

    Added bulletScript.

---
+++ Uranium/Assets/bulletScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScript : MonoBehaviour {
+    public float lifeTimer;
+    public float lifeLimit;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lifeCycle();
+	}
+
+    void lifeCycle()
+    {
+        lifeTimer += Time.deltaTime;
+        if (lifeTimer >= lifeLimit)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
