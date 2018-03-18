81029a9fb80883dc5b22bda036674969fabba235
PeteyWoufes <30848697+PeteyWoufes@users.noreply.github.com>
2018-03-18 15:24:59 +0000

    Added autoRotate.

---
+++ Uranium/Assets/autoRotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class autoRotate : MonoBehaviour {
+    public float rotation;
+	// Use this for initialization
+	void Start () {
+
+        rotation = Random.Range(-3f, 3f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.forward, rotation);
+	}
+}
